22947b231db1662ac3b6a2197178a5ae2727a421
Camila Claudino <camila.claudinoo123@gmail.com>
2024-01-30 09:08:27 -0300

    correção na função de autenticação

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def authenticate_api_key(api_key):
     return api_key in app.config['VALID_API_KEYS']
 
 @app.before_request
-def authenticate_api_key():
+def check_api_key():
     if request.endpoint == 'api_summarize':
         api_key = request.headers.get('Api-Key')
+        print(f"Received API key: {api_key}")  
         if not api_key or not authenticate_api_key(api_key):
             return jsonify({'error': 'Unauthorized. Invalid API key.'}), 401
-
+        
 #api routes
 app.add_url_rule('/summarize', 'api_summarize', api_summarize, methods=['POST'])
 app.add_url_rule('/', 'index', index, methods=['GET'])
